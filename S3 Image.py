8d99e84ee715c9b49c03dc5afbb4116175b6390b
Jesse Borg <jborg@gwu.edu>
2020-11-30 20:17:57 -0500

    Update S3 Image.py

--- S3 Image.py
+++ S3 Image.py
@@ -4,9 +4,9 @@ import os
 
 def upload_files(path):
     session = boto3.Session(
-        aws_access_key_id='AKIAJHYGO25OEOQJF7AA',
-        aws_secret_access_key='PT6ZY9iyAWH1pPctTyOTkPSO4/vRDskUMr5wmm4V',
-        region_name='us-east-1'
+        aws_access_key_id='Your_Key_ID',
+        aws_secret_access_key='Your_Access_Key',
+        region_name='Your_Region'
     )
     s3 = session.resource('s3')
     bucket = s3.Bucket('dats6450dogbreedrecognition')
@@ -18,4 +18,4 @@ def upload_files(path):
                 bucket.put_object(Key=full_path[len(path) + 1:], Body=data)
 
 if __name__ == "__main__":
-    upload_files('Dog Images - Full Dataset')
+    upload_files('Your Folder/File Path')
